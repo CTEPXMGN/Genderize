23600e624ec2a48227dbe8ce44aa598d0116c598
CTEPXMGN <98664376+CTEPXMGN@users.noreply.github.com>
2022-03-10 14:01:48 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,57 @@
-const submitBtn = document.querySelector('.button');
-const inputName = document.querySelector('.input');
-const outputName = document.querySelector('.gender');
+let cityName = '';
+const serverUrl = 'https://api.openweathermap.org/data/2.5/weather';
+const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
 
-const serverUrl = 'https://api.genderize.io';
+const findBtn = document.querySelector('.find__btn');
+const inputCity = document.querySelector('.input');
+const outputCity = document.querySelector('.output__city');
+const outputTemp = document.querySelector('.output__temperature');
+const outputIcon = document.querySelector('.output__icon');
+const addLike = document.querySelector('.like__btn');
+const deleteButton = document.querySelectorAll('.delete__btn');
 
-submitBtn.addEventListener('click', async function() {
-    const firstName = inputName.value;
+async function findCity() {
+    if (inputCity.value !== "") {
+        cityName = inputCity.value;
+        const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
+        let response = await fetch(url);
+        if (response.ok) {
+            let json = await response.json();
+            // inputCity.value = '';
+            outputCity.innerHTML = cityName;
+            let temperature = Math.floor(json.main.temp);
+            outputTemp.innerHTML = temperature + `°`;
+            let srcUrl = `http://openweathermap.org/img/wn/${json.weather[0].icon}.png`;
+            outputIcon.src = srcUrl;
+        } else {
+            alert('Введите корректное название');
+            inputCity.value = '';
+        }
+    } else {
+        alert("Введите название города");
+    }
+};
+
+function addCity() {
+    if (inputCity.value !== '') {
+        document.getElementById('ul').insertAdjacentHTML('afterbegin', `
+        <li>${inputCity.value} <input type="button" class="close__btn"></li>
+        `);
+        const elementID = document.getElementById('');
+
+        console.log('like');
+    } else {
+        alert("А что добавить-то?");
+    }
     
-    const url = `${serverUrl}?name=${firstName}`;
-    let response = await fetch(url);
-    
-    let json = await response.json();
-    
-    outputName.textContent = `${firstName}` + ' - ' + `${json.gender}`;
-    
-});
+}
+
+findBtn.addEventListener('click', findCity);
+addLike.addEventListener('click', addCity);
+
+deleteButton.forEach(function(item) {
+    item.onclcik = function () {
+        console.log('delete');
+        item.parentElement.remove();
+    }
+})
